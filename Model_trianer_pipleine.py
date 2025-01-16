b9533813d137be00688be59dc0e7499cb138c742
Siddharth Latthe <121747151+Siddharth-Latthe-07@users.noreply.github.com>
2025-01-16 15:13:52 +0530

    Update Model_trianer_pipleine.py

--- Model_trianer_pipleine.py
+++ Model_trianer_pipleine.py
@@ -6,4 +6,20 @@ def run_model_trainer(config, X_train, y_train):
     model_trainer = ModelTrainer(model_trainer_config)
     model = model_trainer.initiate_model_training(X_train, y_train)
     return model
-  
+
+
+
+
+
+# src/pipeline/model_trainer_pipeline.py
+from src/components.model_trainer import ModelTrainer
+from src.entity.config_entity import ModelTrainerConfig
+
+class ModelTrainerPipeline:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+        self.model_trainer = ModelTrainer(self.config)
+
+    def run(self):
+        self.model_trainer.train_model()
+        
