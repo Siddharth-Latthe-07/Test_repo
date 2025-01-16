1f55755c5c7b58763015b46877463db30f8aed2e
Siddharth Latthe <121747151+Siddharth-Latthe-07@users.noreply.github.com>
2025-01-16 15:13:26 +0530

    Update Data_trnasformation-pipeline.py

--- Data_trnasformation-pipeline.py
+++ Data_trnasformation-pipeline.py
@@ -6,3 +6,18 @@ def run_data_transformation(config, train_data, test_data):
     data_transformation = DataTransformation(data_transformation_config)
     X_train, X_test, y_train, y_test = data_transformation.initiate_data_transformation(train_data, test_data)
     return X_train, X_test, y_train, y_test
+
+
+
+# src/pipeline/data_transformation_pipeline.py
+from src.components.data_transformation import DataTransformation
+from src.entity.config_entity import DataTransformationConfig
+
+class DataTransformationPipeline:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+        self.data_transformation = DataTransformation(self.config)
+
+    def run(self):
+        self.data_transformation.transform_data()
+        
