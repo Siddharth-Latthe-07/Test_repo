6506ae90bd5e82a63cbb911032d238c8f64cc621
Siddharth Latthe <121747151+Siddharth-Latthe-07@users.noreply.github.com>
2025-01-16 15:49:55 +0530

    Update Data-ingestion-pipe.py

--- Data-ingestion-pipe.py
+++ Data-ingestion-pipe.py
@@ -1,25 +1,12 @@
 from src.components.data_ingestion import DataIngestion
 from src.entity.config_entity import DataIngestionConfig
 
-def run_data_ingestion(config):
-    data_ingestion_config = DataIngestionConfig(**config["data_ingestion"])
-    data_ingestion = DataIngestion(data_ingestion_config)
-    train_data, test_data = data_ingestion.initiate_data_ingestion()
-    return train_data, test_data
-
-
-
-
-
-# src/pipeline/data_ingestion_pipeline.py
-from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig
-
 class DataIngestionPipeline:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
-        self.data_ingestion = DataIngestion(self.config)
+        self.data_ingestion = DataIngestion(config)
 
     def run(self):
-        self.data_ingestion.ingest_data()
+        data = self.data_ingestion.ingest_data()
+        return data
         
