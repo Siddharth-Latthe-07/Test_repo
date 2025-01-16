26b965a81382499f37e7c7ee1d0b4c0fc942c4ba
Siddharth Latthe <121747151+Siddharth-Latthe-07@users.noreply.github.com>
2025-01-16 15:14:17 +0530

    Update Modle-eval-pipelkne.py

--- Modle-eval-pipelkne.py
+++ Modle-eval-pipelkne.py
@@ -6,4 +6,19 @@ def run_model_evaluation(config, model, X_test, y_test):
     model_evaluator = ModelEvaluator(model_evaluation_config)
     accuracy = model_evaluator.initiate_model_evaluation(model, X_test, y_test)
     return accuracy
-  
+
+
+
+
+# src/pipeline/model_eval_pipeline.py
+from src/components.model_eval import ModelEvaluation
+from src.entity.config_entity import ModelEvalConfig
+
+class ModelEvalPipeline:
+    def __init__(self, config: ModelEvalConfig):
+        self.config = config
+        self.model_eval = ModelEvaluation(self.config)
+
+    def run(self):
+        self.model_eval.evaluate_model()
+        
