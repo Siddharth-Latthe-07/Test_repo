a0abc2fafe23e89d53e8d4f48a798c931bdf8be0
Siddharth Latthe <121747151+Siddharth-Latthe-07@users.noreply.github.com>
2025-01-16 15:50:35 +0530

    Update Data_validation_pipeline.py

--- Data_validation_pipeline.py
+++ Data_validation_pipeline.py
@@ -1,21 +1,11 @@
 from src.components.data_validation import DataValidation
-from src.entity.config_entity import DataValidationConfig
-
-def run_data_validation(config, train_data):
-    data_validation_config = DataValidationConfig(**config["data_validation"])
-    data_validation = DataValidation(data_validation_config, train_data)
-    data_validation.initiate_data_validation()
-  
-
-# src/pipeline/data_validation_pipeline.py
-from src.components.data_validation import DataValidation
-from src.entity.config_entity import DataValidationConfig
 
 class DataValidationPipeline:
-    def __init__(self, config: DataValidationConfig):
-        self.config = config
-        self.data_validation = DataValidation(self.config)
+    def __init__(self, data):
+        self.data = data
+        self.data_validation = DataValidation(data)
 
     def run(self):
-        self.data_validation.validate_data()
+        validated_data = self.data_validation.validate_data()
+        return validated_data
         
